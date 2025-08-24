152ec28f2e79ee4a6b92dd623989f2cd3db84ff6
ben8622 <benjaminjknight@gmail.com>
2025-08-24 07:16:39 -0500

    got tx working for esp32 using uart

---
+++ esp32-firmware/src/main.c
@@ -0,0 +1,95 @@
+// #include <stdio.h>
+// #include "esp_log.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+
+// static const char* TAG = "ESP";
+
+// void app_main() {
+//     while (true) {
+//         ESP_LOGI(TAG,"heartbeat", gen_rand_num(1, 100));
+//         vTaskDelay(3000 / portTICK_PERIOD_MS);
+//     }
+// }
+
+
+/* UART asynchronous example, that uses separate RX and TX tasks
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+// #define TXD_PIN (CONFIG_EXAMPLE_UART_TXD) REWROTE
+// #define RXD_PIN (CONFIG_EXAMPLE_UART_RXD) REWROTE
+#define TXD_PIN (GPIO_NUM_1) 
+#define RXD_PIN (GPIO_NUM_3) 
+#define UART_NUM (UART_NUM_1) // ran into issues trying to use uart_0
+
+void init(void)
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, RX_BUF_SIZE * 2, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+}
+
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    return txBytes;
+}
+
+static void tx_task(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1) {
+        sendData(TX_TASK_TAG, "Hello world");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+// static void rx_task(void *arg)
+// {
+//     static const char *RX_TASK_TAG = "RX_TASK";
+//     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+//     uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
+//     while (1) {
+//         const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+//         if (rxBytes > 0) {
+//             data[rxBytes] = 0;
+//             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+//             ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+//         }
+//     }
+//     free(data);
+// }
+
+void app_main(void)
+{
+    init();
+    // xTaskCreate(rx_task, "uart_rx_task", 3072, NULL, configMAX_PRIORITIES - 1, NULL);
+    xTaskCreate(tx_task, "uart_tx_task", 3072, NULL, configMAX_PRIORITIES - 2, NULL);
+}
